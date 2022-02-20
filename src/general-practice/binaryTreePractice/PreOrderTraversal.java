09a98e450120a6a308efe851291e4d8fef455163
nidhii <nshah@miraclesoft.com>
2022-02-20 12:54:08 -0500

    testbtree

---
+++ src/general-practice/binaryTreePractice/PreOrderTraversal.java
@@ -0,0 +1,11 @@
+package binaryTreePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> integerList = new ArrayList<>();
+        return integerList;
+    }
+}
