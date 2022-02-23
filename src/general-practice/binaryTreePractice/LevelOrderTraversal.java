8b1a4c28a1208077a7c11f9a02df167731e9e3ac
nidhii <nshah@miraclesoft.com>
2022-02-23 00:57:54 -0500

    adding level order traversal

---
+++ src/general-practice/binaryTreePractice/LevelOrderTraversal.java
@@ -0,0 +1,12 @@
+package binaryTreePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LevelOrderTraversal {
+    static List<List<Integer>> levelOrderList = new ArrayList<>();
+    static public List<List<Integer>> levelOrder(TreeNode root) {
+    return levelOrderList;
+    }
+}
