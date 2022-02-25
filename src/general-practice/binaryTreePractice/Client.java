5eaba5035074506485bbc353aa08932fadf1e82b
nidhii <nshah@miraclesoft.com>
2022-02-24 23:39:05 -0500

    isSymmetric implimented recurrsively

--- src/general-practice/binaryTreePractice/Client.java
+++ src/general-practice/binaryTreePractice/Client.java
@@ -14,18 +14,30 @@ public class Client {
                                     ), new TreeNode(9, null, new TreeNode(10, new TreeNode(11), null)));
 
         TreeNode root2 = new TreeNode(1, null, new TreeNode(2));
+        TreeNode root3 = new TreeNode(1,
+                                    new TreeNode(2,
+                                            new TreeNode(3),
+                                            new TreeNode(4)),
+                                    new TreeNode(2,
+                                            new TreeNode(4),
+                                            new TreeNode(3)));
+//
+//        // pre-order traversal
+//        List<Integer> preOrderedList = PreOrderTraversal.preorderTraversal(root);
+//        // post-order traversal
+//        List<Integer> postOrderedList = PostOrderTraversal.postorderTraversal(root);
+//        // in-order traversal
+//        List<Integer> inOrderedList = InOrderTraversal.inorderTraversal(root);
+//        // level order traversal
+//        List<List<Integer>> levelOrderTraversalList = LevelOrderTraversal.levelOrder(root);
+//
+        TreeMisc treeMisc = new TreeMisc();
+//        // Maximum depth of a tree
+//        int ans = treeMisc.maxDepth(root2);
+//        System.out.println(ans);
 
-        // pre-order traversal
-       // List<Integer> preOrderedList = PreOrderTraversal.preorderTraversal(root);
-        // post-order traversal
-       // List<Integer> postOrderedList = PostOrderTraversal.postorderTraversal(root);
-        // in-order traversal
-      //  List<Integer> inOrderedList = InOrderTraversal.inorderTraversal(root);
-
-        // level order traversal
-      //  List<List<Integer>> levelOrderTraversalList = LevelOrderTraversal.levelOrder(root);
-        MaximumDepthOfATree maxDT = new MaximumDepthOfATree();
-        int ans = maxDT.maxDepth(root2);
-        System.out.println(ans);
+//        // isSymmetric
+//        boolean ans = treeMisc.isSymmetric(root3);
+//        System.out.println(ans);
     }
 }
---
+++ src/general-practice/binaryTreePractice/TreeMisc.java
@@ -0,0 +1,80 @@
+package binaryTreePractice;
+// if there is one and only one node , depth ==1
+public class TreeMisc {
+    private int treeDepth = -1;
+    private int depthCounter =0;
+
+
+    /*
+        if the left sub-tree is a mirror image of right sub-tree -> the binary tree is symmetric
+     */
+    public boolean isSymmetric(TreeNode root) {
+        boolean isSymmetric = false;
+
+            if(root == null){
+                return isSymmetric;
+            }
+
+            if(root.getLeft() == null && root.getRight() == null){
+                isSymmetric = true;
+                return isSymmetric;
+            }
+
+            isSymmetric = isMirror(root.getLeft(), root.getRight());
+
+        return  isSymmetric;
+    }
+
+    private boolean isMirror(TreeNode node1, TreeNode node2){
+        boolean isSymmetric = false;
+        if (node1 == null && node2 == null){
+            return true;
+        }
+
+        if (node1 != null && node2 != null && node1.getVal() == node2.getVal()){
+            if(isMirror(node1.getLeft(), node2.getRight())){
+                if (isMirror(node1.getRight(), node2.getLeft())){
+                    isSymmetric = true;
+                }
+            }
+        }
+        return  isSymmetric;
+    }
+
+
+    /*
+        Count maximum depth of a tree recursively
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            treeDepth =0;
+            return treeDepth;
+        }
+        if (root.getLeft() == null && root.getRight() ==null){
+            treeDepth =1;
+            return treeDepth;
+        }
+        treeDepth = getDepth(root, 1);
+        return treeDepth;
+    }
+
+
+    /*
+        private method to count the depth of the tree recursively
+     */
+    private int getDepth(TreeNode root, int depth){
+        int answer = depth;
+
+        if (root.getLeft() != null){
+            depthCounter = getDepth(root.getLeft(), depth +1);
+            answer = Math.max(answer, depthCounter);
+        }
+
+        if (root.getRight() !=null){
+           depthCounter = getDepth(root.getRight(), depth +1);
+            answer = Math.max(answer, depthCounter);
+        }
+        return answer;
+    }
+
+}
