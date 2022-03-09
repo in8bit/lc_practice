8d19b3bfdb365529f3f747405f20c2a3efa8354a
nidhii <nidhii.mss@gmail.com>
2022-03-09 15:39:54 -0500

    removeDuplicates

---
+++ src/general-practice/RemoveDuplicatesFromArray.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class RemoveDuplicatesFromArray {
+    public static void main(String[] args) {
+        List<Integer> inputList = new ArrayList<Integer>();
+        inputList.add(1);
+        inputList.add(1);
+        inputList.add(2);
+        inputList.add(1);
+        inputList.add(80);
+        inputList.add(20);
+        inputList.add(1);
+        inputList.add(80);
+
+        List<Integer> answerList = removeDuplicates(inputList);
+        for(Integer i : answerList){
+            System.out.println(i);
+        }
+    }
+    public static List<Integer> removeDuplicates(List<Integer> inputList){
+        List<Integer> answerList = new ArrayList<>();
+//        Set<Integer> set = new HashSet<Integer>(inputList);
+//        answerList.addAll(set);
+//        LinkedHashSet<Integer> set = new LinkedHashSet<>(inputList);
+//        answerList.addAll(set);
+        SortedSet<Integer> set = new TreeSet<>(inputList);
+        answerList.addAll(set);
+        
+        return answerList;
+    }
+
+}
---
+++ src/general-practice/recursion/LinkedListReversal.java
@@ -0,0 +1,8 @@
+package recursion;
+
+public class LinkedListReversal {
+    public static void main(String[] args) {
+
+    }
+
+}
